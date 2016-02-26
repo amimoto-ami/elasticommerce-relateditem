d147188007a0a18ed569f8beb01c33bab57da007
hideokamoto <hide.okamoto@digitalcube.jp>
2016-02-26 16:05:49 +0900

    init

---
+++ elasticommerce-relateditem.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Elasticommerce-relateditem
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: elasticommerce-relateditem
+ * Domain Path: /languages
+ * @package Elasticommerce-relateditem
+ */
